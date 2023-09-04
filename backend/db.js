4dbd13be4b08f80382b1777216f0365955866aa6
Srajan <srajan2303@gmail.com>
2023-09-04 13:34:59 +0530

    inotebook

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook"
+
+async function connectToMongo() {
+    await mongoose.connect(mongoURI)
+    .then(()=> console.log("Connected to Mongo Successfully"))
+    .catch(err => console.log(err));
+    }
+
+module.exports= connectToMongo;
+
+
